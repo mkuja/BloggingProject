e8c0823d1794859de3ff2d348b44e17c44ddbe4d
Mikko Kujala <m.kujala@live.com>
2022-07-21 11:23:26 +0300

    [ADD] Endpoint for refreshing token.

--- backend/blogging/api/auth.py
+++ backend/blogging/api/auth.py
@@ -2,7 +2,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint
 from blogging.marshalling.schemas import Auth
 
-from ..auxialiry.auth import login, logout
+from ..auxialiry.auth import login, logout, refresh
 
 blp = Blueprint("Auth", "auth", url_prefix="/api/v1/auth", description="Operations on users")
 
@@ -17,9 +17,25 @@ class Login(MethodView):
         """Login.
 
         Login user with username and password."""
-        if token := login(**data):
-            return {"token": token}, 200
+        if tokens := login(**data):
+            return tokens, 200
         return {"message": "Invalid username or password."}, 401
+
+
+@blp.route("/refresh")
+class Refresh(MethodView):
+    """Token refreshing."""
+
+    @blp.response(200, Auth)
+    @blp.response(401)
+    def get(self):
+        """Refresh token.
+
+        Create a new token with refresh token. This new token can be used to
+        access non-critical endpoints. For critical endpoints the user must
+        have logged in recently with their credentials."""
+        result = refresh()
+        return result, 200 if "token" in result else 401
 
 
 @blp.route("/logout")
--- backend/blogging/auxialiry/auth.py
+++ backend/blogging/auxialiry/auth.py
@@ -1,10 +1,13 @@
 import datetime
 import time
 from datetime import timedelta
-from typing import Union
+from typing import Union, Dict
 
 from dependency_injector.wiring import Provide
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import (
+    create_access_token, jwt_required, get_jwt_identity,
+    create_refresh_token, get_jwt_header
+)
 from passlib.hash import bcrypt
 
 from app import jwt
@@ -20,7 +23,8 @@ from blogging.services import SessionService
 
 def login(username,
           password,
-          session_service=Provide[Container.session_service]) -> Union[str | bool]:
+          session_service=Provide[Container.session_service]
+          ) -> Union[Dict[str, str] | bool]:
     """Return a fresh JWT on successful login, False otherwise."""
 
     stmt = (select(User)
@@ -31,11 +35,28 @@ def login(username,
             return False
         if bcrypt.verify(password, user_from_db.password_hash):
             claims = {"is_author": user_from_db.is_author}
-            return create_access_token(identity=user_from_db.email,
-                                       fresh=True,
-                                       expires_delta=timedelta(minutes=15),
-                                       additional_claims=claims)
+            return {"token": create_access_token(identity=user_from_db.email,
+                                                 fresh=True,
+                                                 expires_delta=timedelta(minutes=15),
+                                                 additional_claims=claims),
+                    "refresh_token": create_refresh_token(identity=user_from_db.email,
+                                                          additional_claims=claims)}
         return False
+
+
+def refresh(ssession: SessionService = Provide[Container.session_service]):
+    identity = get_jwt_identity()
+    stmt = (select(User)
+            .where(User.email == identity))
+    with ssession.session as session:
+        user = session.scalar(stmt)
+        if not user:
+            return {"message": "Error: No such user."}
+        claims = {"is_author": user.is_author}
+        token = create_access_token(fresh=False,
+                                    expires_delta=timedelta(minutes=20),
+                                    additional_claims=claims)
+        return {"token": token}
 
 
 @jwt_required()
@@ -66,4 +87,3 @@ def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
     if not user:
         return False
     return iat < user["last_logged_out"]
-
